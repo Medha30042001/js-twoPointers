3e9c245726899aaa966f0392eb731f5c47d6dcdf
Medha30042001 <83180508+Medha30042001@users.noreply.github.com>
2025-11-01 13:51:30 +0530

    Update 07-lifeboats.js

--- 07-lifeboats.js
+++ 07-lifeboats.js
@@ -1,14 +1,18 @@
 //gpt taught me
 
-function solve(N,weight,arr){
-  let left = 0, right = N-1, count=0;
-  arr.sort((a, b) => a-b);
+function solve(N, weight, arr) {
+  let left = 0, right = N - 1, count = 0;
+  arr.sort((a, b) => a - b); // sort by weight ascending
   
-  while(left<=right){
-    if(arr[left]+arr[right] <= weight){
-      left++;  
+  while (left <= right) {
+    if (arr[left] + arr[right] <= weight) {
+      // If lightest + heaviest can fit together, send them together
+      left++;
     }
-    right--; count++;
+    // Heaviest person always goes (either alone or with someone)
+    right--;
+    count++;
   }
+  
   console.log(count);
 }
